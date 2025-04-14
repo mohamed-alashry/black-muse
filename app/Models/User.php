70c7b7aaecb7beca249c42e0e67863c9e9f69bf6
mohamed.ashry <mohamed.reda@safavisa.com>
2025-04-14 21:23:53 +0200

    only active admins can login.

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,6 +50,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(\Filament\Panel $panel): bool
     {
-        return true;
+        return $this->status == 'active';
     }
 }
