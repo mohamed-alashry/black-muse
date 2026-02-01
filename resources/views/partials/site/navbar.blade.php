8e32a0a3e87ca06f349c9768b26c6e3f307fd626
mohamed.ashry <mohamed.reda@safavisa.com>
2026-02-01 11:45:01 +0200

    fix styles

--- resources/views/partials/site/navbar.blade.php
+++ resources/views/partials/site/navbar.blade.php
@@ -1,101 +1,110 @@
 <!-- main navbar -->
 <nav class="navbar navbar-expand-sm bg-main border-body pt-4 opacity-75 position-absolute w-100 z-3" data-bs-theme="dark">
-  <div class="container">
-    <a class="navbar-brand" href="{{ route('site.home') }}">
-      <img src="{{ asset('images/mainLogo.svg') }}" alt="Logo" width="122" height="86">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler"
-      aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+    <div class="container">
+        <a class="navbar-brand" href="{{ route('site.home') }}">
+            <img src="{{ asset('images/mainLogo.svg') }}" alt="Logo" width="122" height="86">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler"
+            aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <div class="collapse navbar-collapse d-md-flex flex-md-column align-items-end flex-sm-column-reverse" id="navbarToggler">
+        <div class="collapse navbar-collapse d-md-flex flex-md-column align-items-end flex-sm-column-reverse"
+            id="navbarToggler">
 
-      @if (auth('client')->check())
-        <div class="dropdown">
-          <div class="d-flex align-items-center gap-1 btn dropdown-toggle" type="button" data-bs-toggle="dropdown"
-            aria-expanded="false">
-            <div class="h-100 w-100 overflow-hidden">
-              <img src="{{ asset('images/profile.png') }}" alt="">
-            </div>
-            <p class="text-white fw-light d-flex gap-1 align-items-center">
-              {{ __('site.welcome') }}, <span class="fw-bold">{{ auth('client')->user()->name }}</span>
-              <i class="fas fa-chevron-down dropdown-icon"></i>
-            </p>
-          </div>
-          <ul class="dropdown-menu bg-main">
-            <li>
-              <a href="{{ route('site.profile') }}" class="dropdown-item" type="button">
-                <i class="fa-regular fa-user"></i> {{ __('site.my_profile') }}
-              </a>
-            </li>
-            <li><hr class="dropdown-divider"></li>
-            <li>
-              <form action="{{ route('site.logout') }}" method="POST">
-                @csrf
-                <button class="dropdown-item" type="submit">
-                  <i class="fa-solid fa-arrow-right-from-bracket"></i> {{ __('site.logout') }}
-                </button>
-              </form>
-            </li>
-          </ul>
+            @if (auth('client')->check())
+                {{-- <div class="d-flex align-items-center gap-3 ms-auto"> --}}
+                <div class="d-flex align-items-center gap-3 ms-auto ms-md-auto locale-user-wrap">
+
+                    <div class="dropdown">
+                        <div class="d-flex align-items-center gap-1 btn dropdown-toggle" type="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <div class="h-100 w-100 overflow-hidden">
+                                <img src="{{ asset('images/profile.png') }}" alt="">
+                            </div>
+                            <p class="text-white fw-light d-flex gap-1 align-items-center">
+                                {{ __('site.welcome') }}, <span
+                                    class="fw-bold">{{ auth('client')->user()->name }}</span>
+                                <i class="fas fa-chevron-down dropdown-icon"></i>
+                            </p>
+                        </div>
+                        {{-- <ul class="dropdown-menu bg-main"> --}}
+                        <ul class="dropdown-menu bg-main dropdown-menu-end dropdown-menu-start">
+                            <li>
+                                <a href="{{ route('site.profile') }}" class="dropdown-item" type="button">
+                                    <i class="fa-regular fa-user"></i> {{ __('site.my_profile') }}
+                                </a>
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <form action="{{ route('site.logout') }}" method="POST">
+                                    @csrf
+                                    <button class="dropdown-item" type="submit">
+                                        <i class="fa-solid fa-arrow-right-from-bracket"></i> {{ __('site.logout') }}
+                                    </button>
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <form method="get"
+                        action="{{ LaravelLocalization::getLocalizedURL(app()->getLocale() == 'ar' ? 'en' : 'ar') }}">
+                        <button type="submit" class="btn text-white d-flex align-items-center gap-1">
+                            <i class="fa-solid fa-globe"></i> {{ __('site.language') }}
+                        </button>
+                    </form>
+                </div>
+            @else
+                <div class="d-flex gap-2">
+                    <button class="btn btn-outline-light rounded-4" data-bs-toggle="modal" data-bs-target="#loginModal">
+                        <i class="fa-solid fa-arrow-right-to-bracket"></i> {{ __('site.login') }}
+                    </button>
+                    <button class="btn bg-white text-black rounded-4" data-bs-toggle="modal"
+                        data-bs-target="#registerModal">
+                        <i class="fa-solid fa-user-plus"></i> {{ __('site.register') }}
+                    </button>
+
+                    <form method="get"
+                        action="{{ LaravelLocalization::getLocalizedURL(app()->getLocale() == 'ar' ? 'en' : 'ar') }}">
+                        <button type="submit" class="text-white btn">
+                            <i class="fa-solid fa-globe"></i> {{ __('site.language') }}
+                        </button>
+                    </form>
+                </div>
+            @endif
+
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="{{ route('site.home') }}">{{ __('site.home') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="{{ route('site.home') }}#services">
+                        {{ __('site.services') }}
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="{{ route('site.home') }}#binderies">
+                        {{ __('site.binderies') }}
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="{{ route('site.home') }}#protofolios">
+                        {{ __('site.portfolio') }}
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="{{ route('site.gallery') }}">{{ __('site.gallery') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="{{ route('site.about') }}">{{ __('site.about') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="{{ route('site.contact') }}">{{ __('site.contact') }}</a>
+                </li>
+            </ul>
+
         </div>
-
-        <div class="d-flex gap-2">
-          <form method="get" action="{{ LaravelLocalization::getLocalizedURL(app()->getLocale() == 'ar' ? 'en' : 'ar') }}">
-            <button type="submit" class="text-white btn">
-              <i class="fa-solid fa-globe"></i> {{ __('site.language') }}
-            </button>
-          </form>
-        </div>
-        
-      @else
-        <div class="d-flex gap-2">
-          <button class="btn btn-outline-light rounded-4" data-bs-toggle="modal" data-bs-target="#loginModal">
-            <i class="fa-solid fa-arrow-right-to-bracket"></i> {{ __('site.login') }}
-          </button>
-          <button class="btn bg-white text-black rounded-4" data-bs-toggle="modal" data-bs-target="#registerModal">
-            <i class="fa-solid fa-user-plus"></i> {{ __('site.register') }}
-          </button>
-
-          <form method="get" action="{{ LaravelLocalization::getLocalizedURL(app()->getLocale() == 'ar' ? 'en' : 'ar') }}">
-            <button type="submit" class="text-white btn">
-              <i class="fa-solid fa-globe"></i> {{ __('site.language') }}
-            </button>
-          </form>
-        </div>
-      @endif
-
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link text-white" href="{{ route('site.home') }}">{{ __('site.home') }}</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="@if(Route::currentRouteName() !== 'site.home'){{ route('site.home') }}@endif#services">
-            {{ __('site.services') }}
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="@if(Route::currentRouteName() !== 'site.home'){{ route('site.home') }}@endif#binderies">
-            {{ __('site.binderies') }}
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="@if(Route::currentRouteName() !== 'site.home'){{ route('site.home') }}@endif#protofolios">
-            {{ __('site.portfolio') }}
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="{{ route('site.gallery') }}">{{ __('site.gallery') }}</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="{{ route('site.about') }}">{{ __('site.about') }}</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="{{ route('site.contact') }}">{{ __('site.contact') }}</a>
-        </li>
-      </ul>
-
     </div>
-  </div>
 </nav>
--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -5,147 +5,144 @@
 @section('meta_description', $homePage->meta_desc)
 
 @section('content')
-<button id="backToTopBtn" title="@lang('site.go_to_top')">↑</button>
+    <button id="backToTopBtn" title="@lang('site.go_to_top')">↑</button>
 
-<div class="position-relative">
-    @include('partials.site.navbar')
-    <section class="hero">
-       <video autoplay muted loop preload="auto" playsinline >
-         <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
-       </video>
-    </section>
-</div>
+    <div class="position-relative">
+        @include('partials.site.navbar')
+        <section class="hero">
+            <video autoplay muted loop preload="auto" playsinline>
+                <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
+            </video>
+        </section>
+    </div>
 
-<!-- Services section -->
-<section class="bg-main py-5" id="services">
-    <div class="container">
-        <div class="d-flex justify-content-between align-items-center mb-4">
-            <div class="text-white fw-lighter">
-                <h2 class="fw-lighter">@lang('site.our') <strong class="text-gold">Black Muse</strong> @lang('site.our_services')</h2>
-                <p class="fs-4">@lang('site.what_we_offer')</p>
+    <!-- Services section -->
+    <section class="bg-main py-5" id="services">
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center mb-4">
+                <div class="text-white fw-lighter">
+                    <h2 class="fw-lighter">@lang('site.our') <strong class="text-gold">Black Muse</strong> @lang('site.our_services')
+                    </h2>
+                    <p class="fs-4">@lang('site.what_we_offer')</p>
+                </div>
             </div>
-        </div>
 
-        <div class="services-carousel owl-carousel">
-            @foreach($services->where('category', 'photography') as $service)
-                <div class="service-card">
-                    <img src="{{ asset('storage/'.$service->photo) }}" alt="imagecard">
-                    <div class="info-service px-1 py-3">
-                        <h5>{{$service->name}}</h5>
-                        <button
-                          type="button"
-                          class="btn btn-service"
-                          @if(auth('client')->check())
-                            data-bs-toggle="modal"
+            <div class="services-carousel owl-carousel">
+                @foreach ($services->where('category', 'photography') as $service)
+                    <div class="service-card">
+                        <img src="{{ asset('storage/' . $service->photo) }}" alt="imagecard">
+                        <div class="info-service px-1 py-3">
+                            <h5>{{ $service->name }}</h5>
+                            <button type="button" class="btn btn-service"
+                                @if (auth('client')->check()) data-bs-toggle="modal"
                             data-bs-target="#calendarModalToggle"
                             data-service-id="{{ $service->id }}"
                           @else
                             data-bs-toggle="modal"
-                            data-bs-target="#loginModal"
-                          @endif
-                        >
-                          @lang('site.choose_package_now')
-                        </button>
-                    </div>
-                </div>
-              @endforeach
-        </div>
-    </div>
-</section>
-
-<!-- Portfolio photography section -->
-<section class="bg-main py-3 porto-section" id="protofolios"
-         style="background-image: url({{asset('images/portohero.png')}});">
-    <div class="row h-100">
-        <div class="col-md-4 my-auto">
-            <div class="porto-summry text-white d-flex flex-column gap-2 z-3 mb-2 mx-md-5 mx-2">
-                <p class="fw-lighter">@lang('site.portfolio_category')</p>
-                <h1 class="text-gold lh-1">Black Muse</h1>
-                <h1 class="fw-bolder lh-1">@lang('site.portfolio_photography')</h1>
-               {{-- <p class="fw-lighter">@lang('site.portfolio_description')</p> --}}
-            </div>
-        </div>
-        <div class="col-md-8 mt-auto ps-md-5 mb-4">
-            <div class="portfolio-carousel owl-carousel z-3">
-                @foreach($portfolios->where('category', 'photography') as $portfolio)
-                    <div class="porto-card ">
-                        <a href="{{ route('site.portfolio.show', $portfolio->id) }}">
-                            <img class="porto-img" src="{{ asset('storage/'.$portfolio->photo) }}" alt="imagecard">
-                            <p class="porto-title w-75 text-wrap">{{$portfolio->title}}</p>
-                        </a>
+                            data-bs-target="#loginModal" @endif>
+                                @lang('site.choose_package_now')
+                            </button>
+                        </div>
                     </div>
                 @endforeach
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
-<!-- Portfolio bindery section -->
-<section class="bg-main pt-5">
-    <div class="container" id="binderies">
-        <div class="d-flex justify-content-between align-items-center mb-4">
-            <div class="text-white fw-lighter">
-                <h2 class="fw-lighter">@lang('site.our') <strong class="text-gold">Black Muse</strong> @lang('site.portfolio_bindery')</h2>
-            </div>
-        </div>
-
-        <div class="portfolio-carousel owl-carousel">
-            @foreach($portfolios->where('category', 'bindery') as $portfolio)
-                <div class="service-card">
-                 <a href="{{ route('site.portfolio.show', $portfolio->id) }}">
-                    <img src="{{ asset('storage/'.$portfolio->photo) }}" alt="imagecard">
-                    <div class="info-service px-1 py-3">
-                        <h5>{{$portfolio->title}}</h5>
-                    </div>
-                  </a>
+    <!-- Portfolio photography section -->
+    <section class="bg-main py-3 porto-section" id="protofolios"
+        style="background-image: url({{ asset('images/portohero.png') }});">
+        <div class="row h-100">
+            <div class="col-md-4 my-auto">
+                <div class="porto-summry text-white d-flex flex-column gap-2 z-3 mb-2 mx-md-5 mx-2">
+                    <p class="fw-lighter">@lang('site.portfolio_category')</p>
+                    <h1 class="text-gold lh-1">Black Muse</h1>
+                    <h1 class="fw-bolder lh-1">@lang('site.portfolio_photography')</h1>
+                    {{-- <p class="fw-lighter">@lang('site.portfolio_description')</p> --}}
                 </div>
-              @endforeach
-        </div>
-    </div><br>
-</section>
-
-<!-- Bindery & Lab section -->
-<section class="bg-main pb-5"><br>
-    <div class="container">
-        <div class="respnsive-section">
-            <div class="fw-lighter">
-                <h2 class="fw-lighter text-white"><strong class="text-gold">@lang('site.bindery_lab')</strong></h2>
-                <p class="fs-4 text-white">@lang('site.explore_more')</p>
             </div>
-            @foreach($services->where('category', '!=', 'photography') as $service)
-                <div class="service-card">
-                    <img src="{{ asset('storage/'.$service->photo) }}" alt="imagecard">
-                    <div class="info-service px-1 py-3">
-                        <h5>{{ $service->name }}</h5>
-                        @if(auth('client')->check())
-                            <a href="{{ route('site.service.packages', $service->id) }}" class="btn btn-service"
-                               type="button">@lang('site.choose_package_now')</a>
-                        @else
-                            <button type="button" class="btn btn-service" data-bs-toggle="modal"
+            <div class="col-md-8 mt-auto ps-md-5 mb-4">
+                <div class="portfolio-carousel owl-carousel z-3">
+                    @foreach ($portfolios->where('category', 'photography') as $portfolio)
+                        <div class="porto-card ">
+                            <a href="{{ route('site.portfolio.show', $portfolio->id) }}">
+                                <img class="porto-img" src="{{ asset('storage/' . $portfolio->photo) }}" alt="imagecard">
+                                <p class="porto-title w-75 text-wrap">{{ $portfolio->title }}</p>
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Portfolio bindery section -->
+    <section class="bg-main pt-5">
+        <div class="container" id="binderies">
+            <div class="d-flex justify-content-between align-items-center mb-4">
+                <div class="text-white fw-lighter">
+                    <h2 class="fw-lighter">@lang('site.our') <strong class="text-gold">Black Muse</strong> @lang('site.portfolio_bindery')
+                    </h2>
+                </div>
+            </div>
+
+            <div class="portfolio-carousel owl-carousel">
+                @foreach ($portfolios->where('category', 'bindery') as $portfolio)
+                    <div class="service-card">
+                        <a href="{{ route('site.portfolio.show', $portfolio->id) }}">
+                            <img src="{{ asset('storage/' . $portfolio->photo) }}" alt="imagecard">
+                            <div class="info-service">
+                                <h5>{{ $portfolio->title }}</h5>
+                            </div>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        </div><br>
+    </section>
+
+    <!-- Bindery & Lab section -->
+    <section class="bg-main pb-5"><br>
+        <div class="container">
+            <div class="respnsive-section">
+                <div class="fw-lighter">
+                    <h2 class="fw-lighter text-white"><strong class="text-gold">@lang('site.bindery_lab')</strong></h2>
+                    <p class="fs-4 text-white">@lang('site.explore_more')</p>
+                </div>
+                @foreach ($services->where('category', '!=', 'photography') as $service)
+                    <div class="service-card">
+                        <img src="{{ asset('storage/' . $service->photo) }}" alt="imagecard">
+                        <div class="info-service">
+                            <h5>{{ $service->name }}</h5>
+                            @if (auth('client')->check())
+                                <a href="{{ route('site.service.packages', $service->id) }}" class="btn btn-service"
+                                    type="button">@lang('site.choose_package_now')</a>
+                            @else
+                                <button type="button" class="btn btn-service" data-bs-toggle="modal"
                                     data-bs-target="#loginModal">
-                                @lang('site.choose_package_now')
-                            </button>
-                        @endif
+                                    @lang('site.choose_package_now')
+                                </button>
+                            @endif
+                        </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 
-<!-- Sections from DB -->
-@foreach($homePage->sections as $key => $section)
-    @if($key % 2 == 0)
-        <section class="about-section py-5 bg-main">
-    @else
-        <section class="about-section py-5" style="background-image: url({{asset('images/about-bg.png')}});">
-    @endif
+    <!-- Sections from DB -->
+    @foreach ($homePage->sections as $key => $section)
+        @if ($key % 2 == 0)
+            <section class="about-section py-5 bg-main">
+            @else
+                <section class="about-section py-5" style="background-image: url({{ asset('images/about-bg.png') }});">
+        @endif
         <div class="container d-flex flex-column gap-3 text-white">
             {!! $section->content !!}
         </div>
-    </section>
-@endforeach
+        </section>
+    @endforeach
 
-@include('partials.site.modals.booking_date')
+    @include('partials.site.modals.booking_date')
 
 @endsection
